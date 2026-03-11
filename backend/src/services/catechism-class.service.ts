5307ca8dd9b57f93452cf3b2e0c58f51e7147c32
Matheus Alves <matheusrike.alves@gmail.com>
2026-03-11 11:39:00 -0300

    refactor: improve create method and validation logic in CatechismClassService

--- backend/src/services/catechism-class.service.ts
+++ backend/src/services/catechism-class.service.ts
@@ -1,9 +1,12 @@
+import { DaysOfWeek } from '@/entities/catechism-class.entity';
+import { User } from '@/entities/user.entity';
 import { ICatechismClassRepository } from '@/repositories/catechism-class.repository';
 import { IUserRepository } from '@/repositories/user.repository';
 import {
 	CreateCatechismClass,
 	FindAllCatechismClassFilters,
 } from '@/schemas/catechism-class.schema';
+import { Id } from '@/schemas/common.schema';
 
 export class CatechismClassService {
 	constructor(
@@ -12,37 +15,46 @@ export class CatechismClassService {
 	) {}
 
 	public async create(data: CreateCatechismClass) {
+		await this.validateCatechist(data.catechistId);
+		this.checkAgeConsistency(data.minAge, data.maxAge);
+		await this.checkAvailabilityConflicts({ ...data, status: true });
+
+		return this.CatechismClassRepository.create({ ...data, status: true });
+	}
+
+	public async findAll(filters: FindAllCatechismClassFilters) {
+		return this.CatechismClassRepository.findAll(filters);
+	}
+
+	private async validateCatechist(catechistId: Id): Promise<User> {
 		// Verifica se o usuario e um catequista valido
-		const catechist = await this.UserRepository.findById(data.catechistId);
+		const catechist = await this.UserRepository.findById(catechistId);
 
 		if (!catechist || catechist.role !== 'CATECHIST') {
 			throw new Error('Catechist not found');
 		}
 
-		// verifica se as idades estao corretas
-		if (data.minAge <= 0 || data.maxAge > data.minAge) {
-			throw new Error('Inconsistency in the provided age ranges');
-		}
-
-		// Verifica a disponibilidade da sala, catequista e se ha conflitos de horarios
-		const params = {
-			status: true,
-			dayOfWeek: data.dayOfWeek,
-			startTime: data.startTime,
-			endTime: data.endTime,
-			location: data.location,
-			catechistId: data.catechistId,
-		};
-
-		const existingClass =
-			await this.CatechismClassRepository.findConflict(params);
-
-		if (existingClass) throw new Error('Conflict with existing class');
-
-		return this.CatechismClassRepository.create({ ...data, status: true });
+		return catechist;
 	}
 
-	async findAll(filters: FindAllCatechismClassFilters) {
-		return this.CatechismClassRepository.findAll(filters);
+	private checkAgeConsistency(minAge: number, maxAge: number): void {
+		// verifica se as idades estao consistentes
+		if (minAge <= 0 || maxAge > minAge) {
+			throw new Error('Inconsistency in the provided age ranges');
+		}
+	}
+
+	private async checkAvailabilityConflicts(data: {
+		status: boolean;
+		dayOfWeek: DaysOfWeek;
+		startTime: Date;
+		endTime: Date;
+		location: string;
+		catechistId: Id;
+	}) {
+		const existingClass =
+			await this.CatechismClassRepository.findConflict(data);
+
+		if (existingClass) throw new Error('Conflict with existing class');
 	}
 }
